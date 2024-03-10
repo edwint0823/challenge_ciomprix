4d3c9e2ec5a7c43449f28d3eba8917b4d74829fc
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-03-10 11:19:28 -0500

    upload evidence

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
 router.use('/users', require('../../src/user/core/router/user.router'))
 router.use('/subjects', require('../../src/subject/core/router/subject.router'))
+router.use('/evidences', require('../../src/evidence_record/core/router/evidence.router'))
 module.exports = router
---
+++ src/evidence_record/adapters/api/evidence.controller.js
@@ -0,0 +1,27 @@
+const {evidenceUploadCheck} = require("./validators/evidence.validator");
+const {validationResult, matchedData} = require("express-validator");
+const {HttpStatus, errorMessages} = require("../../../../core/constants");
+const {deleteFile} = require("../../../../core/utils");
+const useCase = require("../../core/usecases/evidence.usecase");
+
+const uploadEvidence = async (req, res, next) => {
+    await Promise.all(evidenceUploadCheck.map(validation => validation.run(req)));
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        if (req.file) {
+            deleteFile(req.file.path)
+        }
+        return res.status(HttpStatus.BAD_REQUEST).json({
+            message: errorMessages.validationErrors,
+            errors: errors.array()
+        });
+    }
+    const data = matchedData(req);
+    useCase.uploadEvidence(Number(data.subject_id), req.file, req.user).then((r) => {
+        res.send(r)
+    }).catch((err) => next(err))
+}
+
+module.exports = {
+    uploadEvidence,
+}
